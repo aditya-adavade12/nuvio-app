8513e4ba86a4a2068811c94cc20218485ce1ef64
aditya-adavade12 <adityawork118@gmail.com>
2025-05-03 17:35:14 +0530

    new components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ToolBar from './components/ToolBar';
+import DropDown from './components/DropDown';
 
 export const App = () => {
   return (
     <div>
+      <DropDown />
       <ToolBar />
     </div>
   )
--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export const ToolBar = () => {
   return (
-    <div>
-        <div id="tool-bar">
-            
-        </div>
+    <div className="absolute bottom-0 left-0 right-0 flex justify-center mb-8">
+    <div id="tool-bar" className="w-fit flex flex-row gap-3 px-4 py-3 rounded-lg bg-stone-900 shadow-md">
+      {[
+        { icon: "arrow_selector_tool", label: "Cursor" },
+        { icon: "pan_tool", label: "Free Draw" },
+        { icon: "rectangle", label: "Rectangle" },
+        { icon: "change_history", label: "Triangle" },
+        { icon: "circle", label: "Circle" },
+        { icon: "horizontal_rule", label: "Line" },
+        { icon: "trending_flat", label: "Arrow" },
+      ].map((tool, index) => (
+        <button
+          key={index}
+          className="text-sm rounded-lg p-1 border border-stone-600 flex items-center justify-center cursor-pointer hover:bg-stone-800 transition"
+          title={tool.label}
+        >
+          <span className="material-symbols-outlined text-sm opacity-75">{tool.icon}</span>
+        </button>
+      ))}
     </div>
-  )
-}
+  </div>
+  
+  );
+};
 
 export default ToolBar;
